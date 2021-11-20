3c7434e5b625086133edac01cf50b01e8d58d099
Carmen Yeh <carmenyeh@outlook.com>
2021-11-20 23:39:05 +1100

    Create About.js

---
+++ frontend/src/components/About.js
@@ -0,0 +1,19 @@
+function About(){
+
+return(
+ <div id='about'>
+    
+        <p> UP.LOADED aims to explore the nuances of human behaviours through their digital footprints and experiences.</p>
+
+  <p>This is a series of experiments which explores my behaviours, and things that I’ve put online throughout my life through data analysis such as photos, posts and messages. </p>
+
+  <p>Every experiment is conducted with data chronologically ordered over the last 10 years from past to present as you progress through each one.</p>
+
+  <p>These experiments are accompanied with code poetry which reflects my findings and reflections.</p>
+   
+        </div>
+       )
+    
+}
+
+export default About;

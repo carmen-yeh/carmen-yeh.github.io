681899ff7277c8fe09e0e9bb93a88b188099b16a
Maggie So <unimaieso@gmail.com>
2021-11-21 14:11:53 +1100

    add happy birthday

---
+++ frontend/src/components/BirthdayDot.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import styled, { keyframes } from "styled-components";
+import { bounceIn } from "react-animations";
+
+const Dot = styled.span`
+  height: 10px;
+  width: 10px;
+  background-color: black;
+  border-radius: 50%;
+  display: inline-block;
+`;
+
+const HappyBirthdayText = styled.div`
+  text-align: center;
+  color: black;
+`;
+
+const Bounce = styled.div`
+  animation: 1.5s ${keyframes`${bounceIn}`};
+`;
+
+function BirthdayDot({paddingLeft = 0, divSize = 80}) {
+
+    const [active, setActive] = useState(false);
+
+    return (
+        <div onMouseEnter={() => {setActive(true)}} style={{display:'flex', height:divSize, width: divSize, marginLeft: paddingLeft, justifyContent:'center'}}>
+            {active ? (
+                <Bounce>
+                    <HappyBirthdayText>Happy Birthday!</HappyBirthdayText>
+                </Bounce>
+            ) : (
+                <Dot style={{marginTop:10}}/>
+            )}
+        </div>
+    )
+}
+
+export default BirthdayDot;

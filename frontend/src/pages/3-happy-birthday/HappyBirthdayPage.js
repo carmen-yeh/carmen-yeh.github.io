830cdc6b8abf29a3660071c245fd538d9b2a3d83
Maggie So <unimaieso@gmail.com>
2021-11-21 14:12:03 +1100

    add happy birthday

--- frontend/src/pages/3-happy-birthday/HappyBirthdayPage.js
+++ frontend/src/pages/3-happy-birthday/HappyBirthdayPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import BirthdayDot from "../../components/BirthdayDot";
 import NavigationBar from "../../components/NavigationBar";
 
 function HappyBirthdayPage() {
@@ -9,6 +10,7 @@ function HappyBirthdayPage() {
                 Its ma birthday
             </h1>
             <NavigationBar />
+            <BirthdayDot paddingLeft={100} divSize={100}/>
         </div>
     )
 }

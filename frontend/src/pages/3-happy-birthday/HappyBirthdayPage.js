6adb7106e0b2adec9ebee540b6fa4d495148646a
Carmen Yeh <carmenyeh@outlook.com>
2021-11-21 17:56:45 +1100

    Update HappyBirthdayPage.js

--- frontend/src/pages/3-happy-birthday/HappyBirthdayPage.js
+++ frontend/src/pages/3-happy-birthday/HappyBirthdayPage.js
@@ -2,14 +2,15 @@ import React from "react";
 import BirthdayDot from "../../components/3-happy-birthday/BirthdayDot";
 import BottomNavigation from "../../components/BottomNavigation";
 import NavigationBar from "../../components/NavigationBar";
-
+import "../../pages/3-happy-birthday/HappyBirthday.css"
 function HappyBirthdayPage() {
 
     return(
-        <div>
+        <div id = "happybirthday">
             <h1>
-                Its ma birthday
+                HAPPY.BIRTHDAY
             </h1>
+        <p>HAPPY.BIRTHDAY explores the idea of making meaningful posts through analysing how frequent I post a ‘generic’ birthday message. This experiment was formed through conducting reading analysis of posts where each birthday message is highlighted by a white dot. What counts as a meaningful post?</p>
             <NavigationBar />
             <BirthdayDot paddingLeft={10} divSize={100}/>
             <BirthdayDot paddingLeft={50} divSize={100}/>

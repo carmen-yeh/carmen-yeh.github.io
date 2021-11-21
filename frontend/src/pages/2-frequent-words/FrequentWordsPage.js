a77ccba80fc45d7f11ff41db19901e739d0651be
Carmen Yeh <carmenyeh@outlook.com>
2021-11-21 17:16:47 +1100

    Update FrequentWordsPage.js

--- frontend/src/pages/2-frequent-words/FrequentWordsPage.js
+++ frontend/src/pages/2-frequent-words/FrequentWordsPage.js
@@ -10,13 +10,10 @@ function FrequentWordsPage() {
             <h1>
                 Here are my most frequent words
             </h1>
-<<<<<<< Updated upstream
+
             <NavigationBar />
             <BottomNavigation />
-=======
-         
-        <p>FREQUENT.WORDS explores the most frequently used unique words when I interact with others online. Word analysis was conducted where the frequency of each word is presented and how this pattern has changed.</p>
->>>>>>> Stashed changes
+
         </div>
     )
 }

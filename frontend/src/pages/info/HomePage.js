a425416b9014ec244216248777618b07894383f9
Carmen Yeh <carmenyeh@outlook.com>
2021-11-21 15:59:29 +1100

    Update HomePage.js

--- frontend/src/pages/info/HomePage.js
+++ frontend/src/pages/info/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import NavigationBar from "../../components/NavigationBar";
 import Header from "../../components/info/Header";
 import About from "../../components/info/About";
-import "../../pages/info/styles.css"
+import "../../pages/info/Styles.css"
 import LogoImage from "../../assets/header.png"
 
 function HomePage() {

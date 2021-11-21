5c6eb49f480c2873f721fd0fc46eec0b465d6915
Carmen Yeh <carmenyeh@outlook.com>
2021-11-21 15:23:39 +1100

    Update LoveLanguagePage.js

--- frontend/src/pages/1-love-language/LoveLanguagePage.js
+++ frontend/src/pages/1-love-language/LoveLanguagePage.js
@@ -5,10 +5,12 @@ function LoveLanguagePage() {
 
     return(
         <div>
-            <h1>
-                Lets talk about my love language
-            </h1>
             <NavigationBar />
+        
+         <h1>
+                LOVE.LANGUAGE
+            </h1>
+        <p>LOVE.LANGUAGE explores the idea of expressing love through the sending of images online to loved ones. This is an image analysis of photos I’ve posted online over time where each white square is an image related to food. I highlighted these findings and linked them to the idea of a digital love language.</p>
         </div>
     )
 }

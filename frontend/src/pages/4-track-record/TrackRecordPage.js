e5b726ae78645df03ce003436f9b779de91138c4
Carmen Yeh <carmenyeh@outlook.com>
2021-11-21 18:08:23 +1100

    Update TrackRecordPage.js

--- frontend/src/pages/4-track-record/TrackRecordPage.js
+++ frontend/src/pages/4-track-record/TrackRecordPage.js
@@ -4,15 +4,17 @@ import LineGroup1 from "../../components/4-track-record/LineGroup1";
 import LineGroup2 from "../../components/4-track-record/LineGroup2";
 import LineGroup3 from "../../components/4-track-record/LineGroup3";
 import BottomNavigation from "../../components/BottomNavigation";
+import "../../pages/4-track-record/TrackRecord.css";
 
 function TrackRecordPage() {
 
     return(
-        <div>
-            <h1>
-                Here is my track record
-            </h1>
+        <div id = "trackrecord">
             <NavigationBar />
+            <h1>
+                TRACK.RECORD
+            </h1>
+        <p>TRACK.RECORD explores 3 different personal friendships through sentiment (emotional) analysis of messages exchanged between each of them. The wider the line, the happier the message content is.</p>
             <div style={{display: 'flex', justifyContent: "space-evenly"}}>
                 <LineGroup1 />
                 <LineGroup2 />

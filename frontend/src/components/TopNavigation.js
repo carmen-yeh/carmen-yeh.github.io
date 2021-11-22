f7cc0312f2179584fc376e922730ef3898987182
Carmen Yeh <carmenyeh@outlook.com>
2021-11-22 22:20:13 +1100

    Update TopNavigation.js

--- frontend/src/components/TopNavigation.js
+++ frontend/src/components/TopNavigation.js
@@ -1,5 +1,66 @@
 import React from "react";
+import "../components/TopNavigation.css";
+
 
 function TopNavigation() {
+    return(
+     <div>
+ <navleft>
+        <ul>
+          <li><a href="/">HOME</a></li>
+          <li><a href="love-language">LOVE.LANGUAGE</a></li>
+          <li><a href="frequent-words">FREQUENT.WORDS</a></li>
+          <li><a href="happy-birthday">HAPPY.BIRTHDAY</a></li>
+         <li><a href="track-record">TRACK.RECORD</a></li>
+        </ul>
     
+      </navleft>
+        
+        
+        
+         <navright>
+        <ul>
+          <li><a href="https://projectuploaded.cargo.site">MAKING OF</a></li>
+          <li><a href="https://carmenyeh.com">BACK TO PORTFOLIO</a></li>
+       
+        </ul>
+        
+        
+      </navright>
+         </div>
+        
+        
+        
+        
+        
+        
+        
+        
+//<div id = "navi">
+//        
+//<div id= "navileft">
+//        
+//<p> HOME </p>
+//<p>LOVE.LANGUAGE</p>
+//<p>FREQUENT.WORDS</p>
+//<p>LOVE.LANGUAGE</p>
+//<p>HAPPY.BIRTHDAY</p>
+//<p>TRACK.RECORD</p>  
+//        
+//</div>
+//        
+//        
+//<div id= "naviright">
+//        
+//<p>making of</p>
+//<p>back to portfolio</p>
+//     
+//</div>
+//        
+//</div>
+     
+       )
+
 }
+
+export default TopNavigation;

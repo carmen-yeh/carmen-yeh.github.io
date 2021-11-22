d12d00fd9b2add12b5142e0b530297988d31fe21
Carmen Yeh <carmenyeh@outlook.com>
2021-11-22 22:20:17 +1100

    Update LoveLanguagePage.js

--- frontend/src/pages/1-love-language/LoveLanguagePage.js
+++ frontend/src/pages/1-love-language/LoveLanguagePage.js
@@ -1,15 +1,16 @@
 import React from "react";
 import LoveBox from "../../components/1-love-language/LoveBox";
 import BottomNavigation from "../../components/BottomNavigation";
-import NavigationBar from "../../components/NavigationBar";
 import "../../pages/1-love-language/lovelanguage.css";
+import TopNavigation from "../../components/TopNavigation";
 
 function LoveLanguagePage() {
 
     return(
+        <div>
+        <TopNavigation/>
         <div id = 'lovelanguage'>
-            <NavigationBar/>
-        
+            
             <h1>LOVE.LANGUAGE</h1>
             <p>LOVE.LANGUAGE explores the idea of expressing love through the sending of images online to loved ones. This is an image analysis of photos I’ve posted online over time where each white square is an image related to food. I highlighted these findings and linked them to the idea of a digital love language.</p>
         
@@ -21,6 +22,10 @@ function LoveLanguagePage() {
             <LoveBox paddingLeft={40} text={"Thanks"}/>
             <LoveBox paddingLeft={70} text={"Don't forget me"}/>
             <BottomNavigation />
+        <div id= "footer">  
+        <p2> UP.LOADED - Carmen Yeh 2021 </p2>
+                </div>
+        </div>
         </div>
     )
 }

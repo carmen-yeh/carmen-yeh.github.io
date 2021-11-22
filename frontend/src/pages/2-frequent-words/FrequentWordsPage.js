c3c49ee0b6c046f444ac1957db937c5a29b29cc0
Carmen Yeh <carmenyeh@outlook.com>
2021-11-22 22:20:20 +1100

    Update FrequentWordsPage.js

--- frontend/src/pages/2-frequent-words/FrequentWordsPage.js
+++ frontend/src/pages/2-frequent-words/FrequentWordsPage.js
@@ -1,20 +1,22 @@
 import React from "react";
 import BottomNavigation from "../../components/BottomNavigation";
-import NavigationBar from "../../components/NavigationBar";
 import "../../pages/2-frequent-words/FrequentWords.css";
+import TopNavigation from "../../components/TopNavigation";
 
 function FrequentWordsPage() {
 
     return(
         <div id = "frequentwords">
-           <NavigationBar />
+           <TopNavigation/>
             <h1>
                 FREQUENT.WORDS
             </h1>
         <p>FREQUENT.WORDS explores the most frequently used unique words when I interact with others online. Word analysis was conducted where the frequency of each word is presented and how this pattern has changed.</p>
 
             <BottomNavigation />
-
+<div id= "footer">  
+        <p2> UP.LOADED - Carmen Yeh 2021 </p2>
+                </div>
         </div>
     )
 }

f66475475743a449032fa13cf5c4574e288ed23f
Carmen Yeh <carmenyeh@outlook.com>
2021-11-30 23:11:01 +1100

    Update LineGroup1.js

--- frontend/src/components/4-track-record/LineGroup1.js
+++ frontend/src/components/4-track-record/LineGroup1.js
@@ -5,23 +5,23 @@ function LineGroup1() {
 
     return(
         <div>
-            <Line length={350}/>
-            <Line length={200}/>
-            <Line length={275}/>
-            <br/>
-            <Line length={340}/>
-            <Line length={180}/>
-            <Line length={420}/>
+        <Line length={350}/>
+        <Line length={200}/>
+        <Line length={275}/>
+        <br/>
+        <Line length={340}/>
+        <Line length={180}/>
+        <Line length={420}/>
         <Line length={460}/>
         <Line length={200}/>
-             <Line length={350}/>
+        <Line length={350}/>
         <Line length={180}/>
-         <Line length={280}/>
+        <Line length={280}/>
         <Line length={200}/>
-         <Line length={230}/>
+        <Line length={230}/>
         <Line length={240}/>
-           <Line length={240}/>
-         <br/>
+        <Line length={240}/>
+        <br/>
         <Line length={440}/>
         <Line length={400}/>
         <Line length={300}/>
@@ -73,7 +73,7 @@ function LineGroup1() {
         <Line length={360}/>
         <Line length={200}/>
         <Line length={240}/>
-         <Line length={120}/>
+        <Line length={120}/>
         <Line length={210}/>
         <Line length={360}/>
         <Line length={120}/>
@@ -84,8 +84,8 @@ function LineGroup1() {
         <Line length={150}/>
         <Line length={310}/>
         <Line length={240}/>
-         <Line length={220}/>
-         <Line length={200}/>
+        <Line length={220}/>
+        <Line length={200}/>
         <Line length={280}/>
         <Line length={460}/>
         <Line length={120}/>
@@ -99,6 +99,111 @@ function LineGroup1() {
         <Line length={400}/>
         <Line length={260}/>
         <Line length={150}/>
+        <Line length={340}/>
+        <Line length={100}/>
+        <Line length={170}/>
+        <Line length={340}/>
+        <Line length={220}/>
+        <Line length={140}/>
+        <Line length={310}/>
+        <Line length={280}/>
+        <Line length={250}/>
+        <Line length={310}/>
+        <br/>
+        <Line length={250}/>
+        <Line length={220}/>
+        <Line length={220}/>
+        <Line length={100}/>
+        <Line length={300}/>
+        <Line length={220}/>
+        <Line length={140}/>
+        <Line length={360}/>
+        <Line length={280}/>
+        <Line length={250}/>
+        <Line length={250}/>
+        <Line length={260}/>
+        <Line length={400}/>
+        <Line length={360}/>
+        <Line length={220}/>
+        <Line length={160}/>
+        <Line length={120}/>
+        <Line length={380}/>
+        <Line length={440}/>
+        <Line length={170}/>
+        <Line length={170}/>
+        <Line length={280}/>
+        <Line length={260}/>
+        <Line length={390}/>
+        <Line length={130}/>
+        <Line length={240}/>
+        <Line length={50}/>
+        <Line length={150}/>
+        <Line length={240}/>
+        <Line length={260}/>
+        <Line length={290}/>
+        <Line length={140}/>
+        <Line length={260}/>
+        <Line length={290}/>
+        <Line length={240}/>
+        <Line length={280}/>
+        <Line length={200}/>
+        <Line length={230}/>
+        <Line length={210}/>
+        <Line length={120}/>
+        <Line length={280}/>
+        <Line length={230}/>
+        <Line length={260}/>
+        <Line length={440}/>
+        <Line length={240}/>
+        <Line length={140}/>
+        <Line length={380}/>
+        <Line length={200}/>
+        <Line length={170}/>
+        <br/>
+        <br/>
+        <Line length={100}/>
+        <Line length={220}/>
+        <Line length={300}/>
+        <Line length={330}/>
+        <Line length={180}/>
+        <Line length={330}/>
+        <Line length={120}/>
+        <Line length={240}/>
+        <Line length={440}/>
+        <Line length={190}/>
+        <Line length={100}/>
+        <Line length={220}/>
+        <Line length={140}/>
+        <Line length={160}/>
+        <Line length={440}/>
+        <Line length={140}/>
+        <Line length={330}/>
+        <Line length={100}/>
+        <Line length={330}/>
+        <Line length={240}/>
+        <Line length={250}/>
+        <Line length={260}/>
+        <Line length={230}/>
+        <Line length={260}/>
+        <Line length={250}/>
+        <Line length={280}/>
+        <Line length={230}/>
+        <Line length={330}/>
+        <Line length={250}/>
+        <Line length={440}/>
+        <Line length={230}/>
+        <Line length={160}/>
+        <Line length={240}/>
+        <Line length={330}/>
+        <Line length={140}/>
+        <Line length={260}/>
+        <Line length={330}/>
+        <Line length={240}/>
+        <Line length={280}/>
+        <Line length={300}/>
+        <Line length={240}/>
+        <Line length={100}/>
+        
 
         </div>
     )

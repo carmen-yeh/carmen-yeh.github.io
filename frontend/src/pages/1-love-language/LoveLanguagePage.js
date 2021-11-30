117e8b20cfbf460131e829670d0e21640921854c
Carmen Yeh <carmenyeh@outlook.com>
2021-11-30 17:16:51 +1100

    Update LoveLanguagePage.js

--- frontend/src/pages/1-love-language/LoveLanguagePage.js
+++ frontend/src/pages/1-love-language/LoveLanguagePage.js
@@ -13,21 +13,20 @@ function LoveLanguagePage() {
             
             <h1>LOVE.LANGUAGE</h1>
             <p>LOVE.LANGUAGE explores the idea of expressing love through the sending of images online to loved ones. This is an image analysis of photos I’ve posted online over time where each white square is an image related to food. I highlighted these findings and linked them to the idea of a digital love language.</p>
-        
-            <LoveBox paddingLeft={10} text={"Love You"} verticalSpace={10}/>
-            <LoveBox paddingLeft={80} text={"Me too"} verticalSpace={10}/>
-            <LoveBox paddingLeft={40} text={"I know you love me already"} verticalSpace={20}/>
-            <LoveBox paddingLeft={50} text={"Have some food"} verticalSpace={5}/>
-            <LoveBox paddingLeft={30} text={"I want fruit"} verticalSpace={50}/>
-            <LoveBox paddingLeft={60} text={"Me too"} verticalSpace={10}/>
-            <LoveBox paddingLeft={40} text={"Thanks"} verticalSpace={40}/>
-            <LoveBox paddingLeft={70} text={"Don't forget me"} verticalSpace={5}/>
+
+                <div style={{display: 'flex'}}>
+            <LoveBox paddingLeft={0} text={"e"} verticalSpace={0}/>
+            <LoveBox paddingLeft={0} text={"w"} verticalSpace={0}/>
+     
+
+        </div>
             <BottomNavigation />
         <div id= "footer">  
         <p2> UP.LOADED - Carmen Yeh 2021 </p2>
                 </div>
         </div>
         </div>
+        
     )
 }
 

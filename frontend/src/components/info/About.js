41697eae78f2534ffebd8ea9bf00f6e3f1731ec9
Maggie So <unimaieso@gmail.com>
2021-12-02 17:51:47 +1100

    indent align

--- frontend/src/components/info/About.js
+++ frontend/src/components/info/About.js
@@ -3,18 +3,18 @@ import "../../components/info/About.css"
 function About(){
 
 return(
- <div id='about'>
+      <div id='about'>
     
-        <p> UP.LOADED aims to explore the nuances of human behaviours through their digital footprints and experiences.</p>
+            <p> UP.LOADED aims to explore the nuances of human behaviours through their digital footprints and experiences.</p>
 
-  <p>This is a series of experiments which explores my behaviours, and things that I’ve put online throughout my life through data analysis such as photos, posts and messages. </p>
+            <p>This is a series of experiments which explores my behaviours, and things that I’ve put online throughout my life through data analysis such as photos, posts and messages. </p>
 
-  <p>Every experiment is conducted with data chronologically ordered over the last 10 years from past to present as you progress through each one.</p>
+            <p>Every experiment is conducted with data chronologically ordered over the last 10 years from past to present as you progress through each one.</p>
 
-  <p>These experiments are accompanied with code poetry which reflects my findings and reflections.</p>
+            <p>These experiments are accompanied with code poetry which reflects my findings and reflections.</p>
    
-        </div>
-       )
+      </div>
+      )
     
 }
 

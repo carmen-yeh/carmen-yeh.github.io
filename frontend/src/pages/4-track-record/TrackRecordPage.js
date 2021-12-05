bc60bffeb918187e76d94d606efd00c774697f93
Maggie So <unimaieso@gmail.com>
2021-12-05 13:11:43 +1100

    track record poem

--- frontend/src/pages/4-track-record/TrackRecordPage.js
+++ frontend/src/pages/4-track-record/TrackRecordPage.js
@@ -5,6 +5,7 @@ import LineGroup3 from "../../components/4-track-record/LineGroup3";
 import BottomNavigation from "../../components/BottomNavigation";
 import "../../pages/4-track-record/trackrecord.css";
 import TopNavigation from "../../components/TopNavigation";
+import poem from "../../assets/trackrecordpoem.png";
 
 function TrackRecordPage() {
 
@@ -20,13 +21,7 @@ function TrackRecordPage() {
                 TRACK.RECORD
             </h1>
             <p>TRACK.RECORD explores 3 different personal friendships through sentiment (emotional) analysis of messages exchanged between each of them. The wider the line, the happier the message content is.</p>
-            {/* <div id = "codepoem">
-                <p>  time (2011 + 10) </p>
-                <p> sentiment.value,time, change; </p>
-                <p> high.low.middle() &#123;</p>
-                <p> ////t on e of   vo i  ce if (communication.digitalised) </p>  perceived.emotions = 100% performed.emotions = 300%        
-                <p> print(record.of.relationship) </p>
-            </div> */}
+            <img src={poem} alt={"poem"} style={{position: "absolute", marginTop: 510, marginLeft: 70}}/>
             
             <div style={{display: 'flex', justifyContent: "space-evenly", position: "relative"}}>
                 <LineGroup1 />

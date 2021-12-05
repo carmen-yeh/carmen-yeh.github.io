e17f97b223bad9043fb8d1ac4b579a9cc9b9deb0
Maggie So <unimaieso@gmail.com>
2021-12-05 13:11:21 +1100

    frequent words

--- frontend/src/pages/2-frequent-words/FrequentWordsPage.js
+++ frontend/src/pages/2-frequent-words/FrequentWordsPage.js
@@ -2,10 +2,15 @@ import React, { useEffect } from "react";
 import BottomNavigation from "../../components/BottomNavigation";
 import "../../pages/2-frequent-words/frequentwords.css";
 import TopNavigation from "../../components/TopNavigation";
+import poem1 from "../../assets/frequentwords1000px.png"
+import poem2 from "../../assets/frequentwords2000px.png"
 
 function FrequentWordsPage() {
 
     useEffect(() => {window.scrollTo(0,0)});
+
+    var size = false;
+    window.innerWidth > 1960 ? (size = true) : (size = false);
 
     return(
         <div id='background-blue'>
@@ -15,7 +20,8 @@ function FrequentWordsPage() {
                     FREQUENT.WORDS
                 </h1>
                 <p>FREQUENT.WORDS explores the most frequently used unique words when I interact with others online. Word analysis was conducted where the frequency of each word is presented and how this pattern has changed.</p>
-
+                {size && <img src={poem2} alt={"poem"} style={{width: "100vw", paddingLeft: 0, paddingRight: 0, paddingTop: 0}} />}
+                {!size && <img src={poem1} alt={"poem"} style={{width:"100vw", paddingLeft: 0, paddingRight: 0, paddingTop: 0}} />}
                 <BottomNavigation colour="blue"/>
                 <div id= "footer">  
                     <p2> UP.LOADED - Carmen Yeh 2021 </p2>

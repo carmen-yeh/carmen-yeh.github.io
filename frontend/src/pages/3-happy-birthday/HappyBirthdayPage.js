bb5521b9b37e156c691f8218f012f6b8396a8152
Maggie So <unimaieso@gmail.com>
2021-12-05 13:18:44 +1100

    happy birthday poem

--- frontend/src/pages/3-happy-birthday/HappyBirthdayPage.js
+++ frontend/src/pages/3-happy-birthday/HappyBirthdayPage.js
@@ -3,6 +3,7 @@ import BirthdayDot from "../../components/3-happy-birthday/BirthdayDot";
 import BottomNavigation from "../../components/BottomNavigation";
 import "../../pages/3-happy-birthday/happybirthday.css";
 import TopNavigation from "../../components/TopNavigation";
+import poem from "../../assets/happybirthdaypoem.png";
 
 function HappyBirthdayPage() {
 
@@ -17,23 +18,26 @@ function HappyBirthdayPage() {
             </h1>
             <p>HAPPY.BIRTHDAY explores the idea of making meaningful posts through analysing how frequent I post a ‘generic’ birthday message. This experiment was formed through conducting reading analysis of posts where each birthday message is highlighted by a white dot. What counts as a meaningful post?</p>
         
-            <BirthdayDot paddingLeft={10} divSize={100} verticalSpace={10}/>
-            <BirthdayDot paddingLeft={40} divSize={100} verticalSpace={10}/>
-            <BirthdayDot paddingLeft={80} divSize={100} verticalSpace={10}/>
-            <BirthdayDot paddingLeft={25} divSize={100} verticalSpace={20}/>
-            <BirthdayDot paddingLeft={7} divSize={100} verticalSpace={80}/>
-            <BirthdayDot paddingLeft={85} divSize={100} verticalSpace={120}/>
-            <BirthdayDot paddingLeft={30} divSize={100} verticalSpace={10}/>
-            <BirthdayDot paddingLeft={75} divSize={100} verticalSpace={80}/>
-            <BirthdayDot paddingLeft={5} divSize={100} verticalSpace={10}/>
-            <BirthdayDot paddingLeft={35} divSize={100} verticalSpace={10}/>
-            <BirthdayDot paddingLeft={10} divSize={100} verticalSpace={50}/>
-            <BirthdayDot paddingLeft={70} divSize={100} verticalSpace={10}/>
-            <BirthdayDot paddingLeft={60} divSize={100} verticalSpace={20}/>
-            <BirthdayDot paddingLeft={80} divSize={100} verticalSpace={0}/>
-            <BirthdayDot paddingLeft={15} divSize={100} verticalSpace={10}/>
-            <BirthdayDot paddingLeft={45} divSize={100} verticalSpace={10}/>
-            <BirthdayDot paddingLeft={90} divSize={100} verticalSpace={0}/>
+            <img src={poem} alt="poem" style={{position: "absolute", paddingTop: 700}} />
+            <div style={{position: "relative"}}>
+                <BirthdayDot paddingLeft={10} divSize={100} verticalSpace={10}/>
+                <BirthdayDot paddingLeft={40} divSize={100} verticalSpace={10}/>
+                <BirthdayDot paddingLeft={80} divSize={100} verticalSpace={10}/>
+                <BirthdayDot paddingLeft={25} divSize={100} verticalSpace={20}/>
+                <BirthdayDot paddingLeft={7} divSize={100} verticalSpace={80}/>
+                <BirthdayDot paddingLeft={85} divSize={100} verticalSpace={120}/>
+                <BirthdayDot paddingLeft={30} divSize={100} verticalSpace={10}/>
+                <BirthdayDot paddingLeft={75} divSize={100} verticalSpace={80}/>
+                <BirthdayDot paddingLeft={5} divSize={100} verticalSpace={10}/>
+                <BirthdayDot paddingLeft={35} divSize={100} verticalSpace={10}/>
+                <BirthdayDot paddingLeft={10} divSize={100} verticalSpace={50}/>
+                <BirthdayDot paddingLeft={70} divSize={100} verticalSpace={10}/>
+                <BirthdayDot paddingLeft={60} divSize={100} verticalSpace={20}/>
+                <BirthdayDot paddingLeft={80} divSize={100} verticalSpace={0}/>
+                <BirthdayDot paddingLeft={15} divSize={100} verticalSpace={10}/>
+                <BirthdayDot paddingLeft={45} divSize={100} verticalSpace={10}/>
+                <BirthdayDot paddingLeft={90} divSize={100} verticalSpace={0}/>
+            </div>
             
             <BottomNavigation colour="red"/>
             <div id= "footer">  

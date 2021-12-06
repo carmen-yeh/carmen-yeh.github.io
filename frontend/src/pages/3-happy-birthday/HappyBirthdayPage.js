1dab2a6b5d0c7992ebe1cb2fa17ddf8d91573d0c
Carmen Yeh <carmenyeh@outlook.com>
2021-12-06 19:19:08 +1100

    Update HappyBirthdayPage.js

--- frontend/src/pages/3-happy-birthday/HappyBirthdayPage.js
+++ frontend/src/pages/3-happy-birthday/HappyBirthdayPage.js
@@ -18,7 +18,7 @@ function HappyBirthdayPage() {
             </h1>
             <p>HAPPY.BIRTHDAY explores the idea of making meaningful posts through analysing how frequent I post a ‘generic’ birthday message. This experiment was formed through conducting reading analysis of posts where each birthday message is highlighted by a white dot. What counts as a meaningful post?</p>
         
-            <img src={poem} alt="poem" style={{position: "absolute", paddingTop: 700, maxWidth: 800}} />
+            <img src={poem} alt="poem" style={{position: "absolute", paddingTop: 1800, maxWidth: 800}} />
             <div style={{position: "relative"}}>
                 <BirthdayDot paddingLeft={10} divSize={100} verticalSpace={10}/>
                 <BirthdayDot paddingLeft={40} divSize={100} verticalSpace={10}/>
